c8e319297325018e9d703bd6c60fdcc76abd6e77
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2021-04-02 01:39:57 +0300

    refactor reducer, state with toolkit

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,23 +1,35 @@
-
-import types from './contacts-types'
+import {createAction} from '@reduxjs/toolkit';
 import shortid from 'shortid';
 
-
-const addContact = (data) => ({
-    type: types.ADD,
+const addContact = createAction('conatcts/add', data => ({
     payload: {
         id: shortid.generate(),
         name: data.name,
         number: data.number,
       }
-})
-const deleteContact = (id) => ({
-    type: types.DELETE,
-    payload: id,
-})
-const changeFilter = value => ({
-    type: types.CHANGE_FILTER,
-    payload: value,
-})
+}));
+const deleteContact = createAction('conatcts/delete');
+
+const changeFilter = createAction('conatcts/changeFilter');
+
 // eslint-disable-next-line 
 export default {addContact, deleteContact, changeFilter};
+
+
+// const addContact = (data) => ({
+//     type: types.ADD,
+//     payload: {
+//         id: shortid.generate(),
+//         name: data.name,
+//         number: data.number,
+//       }
+// });
+
+// const deleteContact = (id) => ({
+//     type: types.DELETE,
+//     payload: id,
+// })
+// const changeFilter = value => ({
+//     type: types.CHANGE_FILTER,
+//     payload: value,
+// })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
-  
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import contactsReducer from './contacts/contacts-reducer';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
 
-const store = createStore(rootReducer, composeWithDevTools());
+const middleware = [...getDefaultMiddleware(), logger];
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+  middleware,
+});
 
 export default store;
